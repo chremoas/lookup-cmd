d89cd2aae555b47eb35d7d38d893c56c26eec3dc
Brian Hechinger <wonko@4amlunch.net>
2021-03-17 16:16:21 -0400

    So many things to check in that I've ignored for so long. I'm a terrible person.

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"fmt"
-	"github.com/chremoas/lookup-cmd/command"
-	esisvc "github.com/chremoas/esi-srv/proto"
+
 	proto "github.com/chremoas/chremoas/proto"
+	esisvc "github.com/chremoas/esi-srv/proto"
 	"github.com/chremoas/services-common/config"
+	chremoasPrometheus "github.com/chremoas/services-common/prometheus"
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/client"
+	"go.uber.org/zap"
+
+	"github.com/chremoas/lookup-cmd/command"
 )
 
-var Version = "SET ME YOU KNOB"
-var service micro.Service
-var name = "lookup"
+var (
+	Version = "SET ME YOU KNOB"
+	service micro.Service
+	name    = "lookup"
+	logger  *zap.Logger
+)
 
 func main() {
+	var err error
+
+	// TODO pick stuff up from the config
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	logger.Info("Initialized logger")
+
+	go chremoasPrometheus.PrometheusExporter(logger)
+
 	service = config.NewService(Version, "cmd", name, initialize)
 
 	if err := service.Run(); err != nil {
